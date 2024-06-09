741c6c86dfc7df714d3493fe4233c519566c2ea1
eugenio socha <eugenio.socha2004@gmail.com>
2024-06-09 16:28:08 -0400

    Adicionado conceitos de declaração de variáveis e constantes na sintaxe da linguagem.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // VARIAVEIS
+
+        // <tipoDaVariavel> <nomeDaVariavel> = <valorAtribuidoOpcional>
+        int anoFabricacao = 2010;
+        double salarioMinimo = 2500.547;
+
+        // Variaveis do tipo float precisam do F no final do valor
+        float pi = 3.14F;
+
+        // CONSTANTES
+
+        // Constantes precisam estar em caixa alta, e antes da declarção do tipo 
+        // vem a palavra chave "final"
+
+        final double VALOR_DE_PI = 3.14;
+        
+
     }
 }
