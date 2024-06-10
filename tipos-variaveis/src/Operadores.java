ff2e3d2b71a25678ae19ac043677985c8380031c
eugenio socha <eugenio.socha2004@gmail.com>
2024-06-09 22:07:33 -0400

    Criada a classe Operadores.java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,14 @@
+public class Operadores{
+    public static void main(String[] args) {
+        // CONCATENANDO 
+        String nomeCompleto = "Eugenio " + "Socha";
+
+        System.out.println(nomeCompleto);
+
+        
+        // OPERADORES UNÁRIOS
+        int numero = -8;
+
+        System.out.println(-numero);
+    }
+}
