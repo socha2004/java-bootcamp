066841075f928a95d5e0a459f72141472850929e
eugenio socha <eugenio.socha2004@gmail.com>
2024-06-13 23:18:39 -0400

    criado projeto Métodos

---
+++ metodos/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+     // Qual a proposta do método?
+     // O método retornará algum valor?
+     // (Caso o método retorne nada, ele será representado pela keyword "void" )
+     // Quais os parâmetros do metodo?
+     // O metodo possui risco de causar exceção?
+     // Qual a visibilidade do método? (public, )
+    }
+
+    public void somar(){
+        // LOGICA
+        return  ;
+    }
+}
